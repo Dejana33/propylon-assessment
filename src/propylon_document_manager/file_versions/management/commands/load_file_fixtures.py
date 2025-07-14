fc6e187fcbf28dcc8112dbff431afc512d606692
d.stanic <d.stanic@levi9.com>
2025-07-15 00:09:15 +0200

    Add management command to load test file fixtures

--- src/propylon_document_manager/file_versions/management/commands/load_file_fixtures.py
+++ src/propylon_document_manager/file_versions/management/commands/load_file_fixtures.py
@@ -1,5 +1,5 @@
-from django.core.management.base import BaseCommand, CommandError
-from propylon_document_manager.file_versions.models import FileVersion
+from django.core.management.base import BaseCommand
+from propylon_document_manager.file_versions.models import FileVersion, User, File
 
 file_versions = [
     'bill_document',
@@ -9,13 +9,19 @@ file_versions = [
 ]
 
 class Command(BaseCommand):
-    help = "Load basic file version fixtures"
+    help = "Load test file fixtures"
 
     def handle(self, *args, **options):
+        user, created = User.objects.get_or_create(email="user1@example.com")
+        if created:
+            user.set_password("user123")
+            user.save()
         for file_name in file_versions:
+            file_obj, _ = File.objects.get_or_create(name=file_name, user=user)
             FileVersion.objects.create(
-                file_name=file_name,
-                version_number=1
+                file_obj=file_obj,
+                version_number=1,
+                user=user,
             )
 
         self.stdout.write(
